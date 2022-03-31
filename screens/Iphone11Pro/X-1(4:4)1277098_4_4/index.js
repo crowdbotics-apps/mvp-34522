7971ff2673be194e59d790db32a3626415b6cdb8
Crowdbotics <team@crowdbotics.com>
2022-03-31 23:51:07 +0000

    Update components

---
+++ screens/Iphone11Pro/X-1(4:4)1277098_4_4/index.js
@@ -0,0 +1,189 @@
+import React from "react"
+import {
+  View,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Text,
+  Button,
+  Switch,
+  TextInput,
+  StyleSheet,
+  ScrollView
+} from "react-native"
+import Icon from "react-native-vector-icons/FontAwesome"
+import { CheckBox } from "react-native-elements"
+import { connect } from "react-redux"
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen"
+import { getNavigationScreen } from "@screens"
+export class Blank extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {}
+  }
+  render = () => (
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      style={styles.ScrollView_1}
+    >
+      <View style={styles.View_2} />
+      <View style={styles.View_4_5}>
+        <Text style={styles.Text_4_5}>FINO Foundation</Text>
+      </View>
+      <ImageBackground
+        source={{
+          uri:
+            "https://s3-us-west-2.amazonaws.com/figma-alpha-api/img/3b6e/1fea/ecd7e6f64f7007d974c1af876b8561f9"
+        }}
+        style={styles.ImageBackground_4_11}
+      />
+      <View style={styles.View_7_13}>
+        <Text style={styles.Text_7_13}>SUBMIT</Text>
+      </View>
+      <View style={styles.View_4_9} />
+      <View style={styles.View_4_10}>
+        <Text style={styles.Text_4_10}>Password</Text>
+      </View>
+      <View style={styles.View_4_7} />
+      <View style={styles.View_4_8}>
+        <Text style={styles.Text_4_8}>Email</Text>
+      </View>
+    </ScrollView>
+  )
+}
+
+const styles = StyleSheet.create({
+  ScrollView_1: { backgroundColor: "rgba(255, 255, 255, 1)" },
+  View_2: { height: hp("111%") },
+  View_4_5: {
+    width: wp("42%"),
+    minWidth: wp("42%"),
+    minHeight: hp("3%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("29%"),
+    top: hp("16%"),
+    justifyContent: "flex-start"
+  },
+  Text_4_5: {
+    color: "rgba(0, 0, 0, 1)",
+    fontSize: 16,
+    fontWeight: "400",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  ImageBackground_4_11: {
+    width: wp("73%"),
+    minWidth: wp("73%"),
+    height: hp("7%"),
+    minHeight: hp("7%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("13%"),
+    top: hp("68%")
+  },
+  View_7_13: {
+    width: wp("21%"),
+    minWidth: wp("21%"),
+    minHeight: hp("3%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("40%"),
+    top: hp("69%"),
+    justifyContent: "flex-start"
+  },
+  Text_7_13: {
+    color: "rgba(255, 255, 255, 1)",
+    fontSize: 16,
+    fontWeight: "400",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_4_9: {
+    width: wp("73%"),
+    minWidth: wp("73%"),
+    height: hp("7%"),
+    minHeight: hp("7%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("13%"),
+    top: hp("51%"),
+    backgroundColor: "rgba(255, 255, 255, 1)",
+    borderColor: "rgba(0, 0, 0, 1)",
+    borderWidth: 1
+  },
+  View_4_10: {
+    width: wp("25%"),
+    minWidth: wp("25%"),
+    minHeight: hp("3%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("13%"),
+    top: hp("47%"),
+    justifyContent: "flex-start"
+  },
+  Text_4_10: {
+    color: "rgba(0, 0, 0, 1)",
+    fontSize: 16,
+    fontWeight: "400",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  },
+  View_4_7: {
+    width: wp("73%"),
+    minWidth: wp("73%"),
+    height: hp("7%"),
+    minHeight: hp("7%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("13%"),
+    top: hp("34%"),
+    backgroundColor: "rgba(255, 255, 255, 1)",
+    borderColor: "rgba(0, 0, 0, 1)",
+    borderWidth: 1
+  },
+  View_4_8: {
+    width: wp("14%"),
+    minWidth: wp("14%"),
+    minHeight: hp("3%"),
+    marginLeft: 0,
+    marginTop: 0,
+    position: "absolute",
+    left: wp("13%"),
+    top: hp("30%"),
+    justifyContent: "flex-start"
+  },
+  Text_4_8: {
+    color: "rgba(0, 0, 0, 1)",
+    fontSize: 16,
+    fontWeight: "400",
+    textAlign: "left",
+    fontStyle: "normal",
+    letterSpacing: 0,
+    textTransform: "none"
+  }
+})
+
+const mapStateToProps = state => {
+  return {}
+}
+const mapDispatchToProps = () => {
+  return {}
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Blank)
